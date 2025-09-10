0f3e75372a5d187deb34eb6a1bf7ea1a6323e5d6
allmight777 <agoliganange15@gmail.com>
2025-09-10 10:38:56 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "✅ Ajustement de Nginx pour Render"
-# Remplacer le port 80 par le port fourni par Render
-sed -i "s/listen 80;/listen ${PORT};/" /etc/nginx/sites-enabled/default.conf
+echo "✅ Installation des dépendances Laravel"
+composer install --no-interaction --prefer-dist --optimize-autoloader
 
-echo "✅ Lancer les migrations et le seeding"
+echo "✅ Création de la table notifications si nécessaire"
+php artisan make:notifications-table
+
+
+echo "✅ Lancement des migrations (fresh + seed)"
 php artisan migrate:fresh --seed --force
 
-echo "✅ Lancer PHP-FPM en arrière-plan"
-php-fpm &
-
-echo "✅ Lancer Nginx au premier plan"
-nginx -g 'daemon off;'
+echo "✅ Lancement du serveur nginx/php-fpm"
+/start.sh
