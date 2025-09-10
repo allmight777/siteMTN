f10c08b82af13026b53011cb954a0c4a8c18cbf0
allmight777 <agoliganange15@gmail.com>
2025-09-10 10:10:28 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,12 +4,13 @@ set -e
 echo "✅ Installation des dépendances Laravel"
 composer install --no-interaction --prefer-dist --optimize-autoloader
 
-echo "✅ Création de la table notifications si nécessaire"
-php artisan make:notifications-table
+echo "✅ Nettoyage des caches"
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
 
-
-echo "✅ Lancement des migrations (fresh + seed)"
+echo "✅ Lancement des migrations et seeds"
 php artisan migrate:fresh --seed --force
 
-echo "✅ Lancement du serveur nginx/php-fpm"
-/start.sh
+echo "✅ Démarrage de Nginx + PHP-FPM"
+exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
